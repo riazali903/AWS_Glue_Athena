9830dc508b02cd06ecd3c62babeee206308dd7df
riazali903 <riazali903.com>
2021-09-21 21:47:48 +0100

    changed from five days to one day

--- weatherdata.py
+++ weatherdata.py
@@ -1,59 +1,52 @@
-import requests
+import requests,json,time,math
 import pandas as pd
 from pprint import pprint
 from datetime import datetime, timedelta
-import time
-import math
+
 
 
 class WeatherData:
+
     def __init__(self):
-        self.five_days = []
-        self.frames = []
+        pass
+        #self.five_days = []
+        #self.frames = []
 
     def set_vars(self):
-        now = datetime.now()
-        for x in range(5):
-            d = now - timedelta(days=x)
-            d = d.strftime("%Y-%m-%d")
-            d1 = time.mktime(datetime.strptime(d,"%Y-%m-%d").timetuple())
-            d1 = math.trunc(d1)
-            self.five_days.append(d1)
+        print('set_vars working')
+
+        print('set_var worked')
 
     def parsing(self):
+        print('into parser')
+        now = datetime.now()
+
+        day = now.strftime("%Y-%m-%d")
+        day = time.mktime(datetime.strptime(day, "%Y-%m-%d").timetuple())
+        day = math.trunc(day)
         lon = -0.12570
         lat = 51.50850
-        api_key = '12379f060b354d4a1edafa28418ae871'
+        api_key = '7d2751fa65dfe0f3c12e2f2728ca2cef'
+        url = f'https://api.openweathermap.org/data/2.5/onecall/timemachine?' \
+              f'lat={lat}&lon={lon}&dt={day}&appid={api_key}'
 
-        for day in self.five_days:
-            url = f'https://api.openweathermap.org/data/2.5/onecall/timemachine?' \
-                  f'lat={lat}&lon={lon}&dt={day}&appid={api_key}'
-
-            res = requests.get(url).json()
-            df = pd.DataFrame(res['hourly'])
-            self.frames.append(df)
+        res = requests.get(url).json()
+        return pd.DataFrame(res['hourly'])
 
     def data_wrangling(self):
-        df = pd.concat(self.frames)
+        df = self.parsing()
         df['dt'] = pd.to_datetime(df['dt'], unit='s')
         df.drop(['dew_point', 'uvi', 'clouds', 'wind_deg', 'wind_gust',
-                            'weather','rain'], axis=1, inplace=True)
+                'weather',], axis=1, inplace=True)
+        df.set_index('dt', inplace=True)
+        df.to_json('weather.json', orient='index', indent=2,
+                   date_format='iso', date_unit='s')
         return df
 
 
-    # hist_weather = f'https://api.openweathermap.org/data/2.5/onecall/timemachine?' \
-    #                f'lat={lat}&lon={lon}&dt={1632092400}&appid={api_key}'
-
-
-    # res = requests.get(url).json()
-    # df = pd.DataFrame(res['hourly'])
-    # print(df.head())
-    # pprint(res)
-
-    #resultframe = pd.concat(frames)
-    #resultframe.pop('rain')
-
 obje = WeatherData()
 obje.set_vars()
-obje.parsing()
 dframe = obje.data_wrangling()
+
+
+
